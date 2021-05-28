0d5ae2c30dbad9c8a41b910ae5618f08e948e82e
Caroline <cdsevangelista@inf.ufpel.edu.br>
2021-05-27 23:16:47 -0300

    WIP spawn idealizado

--- src/mainLib.cpp
+++ src/mainLib.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <list>
 #include "headers.hpp"
+using namespace std;
 
 struct Atrib{
 	int p; // prioridade da tarefa
@@ -21,31 +22,52 @@ struct Trabalho {
 list<trabalho *> trabalhosProntos, trabalhosTerminados;
 int qtdProcVirtuais = 0;
 static pthread_t *pvs; // processadores virtuais
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t unlock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t iniciados = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t finalizados = PTHREAD_MUTEX_INITIALIZER;
 
 void *iniciaPV(void *dta) {
-  void* res;
-  struct Trabalho *t;
-  while () {
+	void* res;
+	struct Trabalho *t;
+	while () {
 
-  }
+	}
 }
 
 int start (int m) {
-  int i, threads;
-  pvs = (pthread_t *) malloc(m * sizeof(pthread_t));
-  for (i=0; i < m; i++) {
-    threads = pthread_create(&pvs[i], NULL, iniciaPV, NULL); //cria thread
-    if (threads != 0) {
-      return 0;
-    }
-  } 
-  return 1;
+	int i, threads;
+	pvs = (pthread_t *) malloc(m * sizeof(pthread_t));
+	qtdProcVirtuais = m;
+	for (i=0; i < m; i++) {
+		threads = pthread_create(&pvs[i], NULL, iniciaPV, NULL); //cria thread
+		if (threads != 0) {
+			return 0;
+		}
+	} 
+	return 1;
 }
 
-void finish();
+void finish(){
+	while (qtdProcVirtuais >= 0) {
+		qtdProcVirtuais--;
+	}
+};
 
-int spawn( struct Atrib* atrib, void *(*t) (void *), void* dta );
+int spawn( struct Atrib* atrib, void *(*t) (void *), void* dta ) {
+	pthread_mutex_lock(&iniciados);
+
+    Trabalho *trab;
+	trab = (Trabalho *)malloc(sizeof(Trabalho));
+	idTrabalho++;
+
+    trab->tid = idTrabalho;
+    trab->f = t;
+    trab->dta = dta;
+
+	prontos.push_front(trab);
+
+	pthread_mutex_unlock(&iniciados);
+
+	return idTrabalho;
+}
 
 int sync( int tId, void** res );
