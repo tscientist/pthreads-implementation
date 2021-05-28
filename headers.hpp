6c28b98395a7306716f2528b7f2aedda7667bb94
Caroline <cdsevangelista@inf.ufpel.edu.br>
2021-05-27 23:52:42 -0300

    WIP removendo src

---
+++ headers.hpp
@@ -0,0 +1,15 @@
+#include <pthread.h>
+
+struct Atrib{
+    int p; //prioridade
+    int c; //custo
+};
+
+int start (int m);
+//lança o núcleo de execução, instanciando os 'M' processadores virtuais - retorno = 0(falha) ou 1(deu bom)
+void finish();
+//retorna após todos os PV terem terminado sua execução
+int spawn( struct Atrib* atrib, void *(*t) (void *), void* dta );
+//lança a execução da tarefa descrita - atrib = atributos(struct), t = função a ser executada, dta = parametros da função, retorno = 0(falha) ou 1(deu bom)
+int sync( int tId, void** res );
+//invoca a thread que deve ser sincronizada - tId = ID da thread, res = resultado da execução da thread, retorno = 0(falha) ou 1(deu bom) 
